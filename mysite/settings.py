808128cdce26f4328973717975f928097b801573
gukobrist <bogestvenniyiri@yandex.ru>
2017-12-12 15:49:20 +0300

    Статья нвоая

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.twitter',
     'allauth.socialaccount.providers.odnoklassniki',
     'allauth.socialaccount.providers.instagram',
-    'allauth.socialaccount.providers.google',
 
 ]
 
