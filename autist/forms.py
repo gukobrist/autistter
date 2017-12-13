17ff906f1e41fef1477601a661ff264257837df3
gukobrist <bogestvenniyiri@yandex.ru>
2017-12-13 13:18:50 +0300

    new update

--- autist/forms.py
+++ autist/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from .models import Post
 
@@ -5,3 +6,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=30, label='Voornaam')
+    last_name = forms.CharField(max_length=30, label='Achternaam')
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
--- autist/models.py
+++ autist/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
 from uuslug import slugify
+from django.contrib.auth.models import User
+from django.db import models
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+import hashlib
 
 
 
@@ -31,4 +36,31 @@ class Post(models.Model):
         self.slug = '{0}-{1}'.format(self.pk, slugify(self.title))  # Статья будет отображаться в виде NN-АА-АААА
         super(Post, self).save()
 
-# Create your models here.
+# модель профиля для отображения информации об авторизированном профиле
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+    def profile_image_url(self):
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+        return "http://www.gravatar.com/avatar/{}?s=40".format(hashlib.md5(self.user.email).hexdigest())
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
